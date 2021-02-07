613676e60c12daaf75be8618675082515f472344
Navaneeth K M <34748927+navaneethkm004@users.noreply.github.com>
2021-02-07 12:29:42 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = async (client) => {
+  console.log(`[API] Logged in as ${client.user.username}`);
+  await client.user.setActivity("streaming competition",{ type: 'COMPETING'});
+};
---
+++ util/error.js
@@ -0,0 +1,8 @@
+const { MessageEmbed } = require("discord.js")
+module.exports = async (text, channel) => {
+    let embed = new MessageEmbed()
+    .setColor("RED")
+    .setDescription(text)
+    .setFooter("Something went wrong :(")
+    await channel.send(embed)
+}
